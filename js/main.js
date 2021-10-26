5e48b39ce7b34824a48baab74fcc9914f6882c8b
Vladislav <vadimromashko@gmail.com>
2021-10-26 21:47:35 +0300

    added nav block and styles

--- js/main.js
+++ js/main.js
@@ -1,15 +1,81 @@
 $(document).ready(() => {
-    $('#products').slick({
+
+    $('.category').click((e) => {
+        let currentElement = $(e.target);
+
+        $('.products-container').hide();
+        let id = currentElement.data('id');
+        $('#' + id).show();
+
+        $('.category').removeClass('active');
+        currentElement.addClass('active');
+
+        $('#' + id + '.products').slick('refresh');
+        $('#' + id + '.products-nav').slick('refresh');
+    });
+
+    $('#burgers-container .products').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
         fade: true,
-        asNavFor: '#products-nav'
+        asNavFor: '#burgers-container .products-nav'
     });
-    $('#products-nav').slick({
+    $('#burgers-container .products-nav').slick({
         slidesToShow: 7,
         slidesToScroll: 7,
-        asNavFor: '#products',
+        asNavFor: '#burgers-container .products',
+        dots: false,
+        centerMode: false,
+        infinite: false,
+        focusOnSelect: true
+    });
+
+    $('#fries-container .products').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '#fries-container .products-nav'
+    });
+    $('#fries-container .products-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        asNavFor: '#fries-container .products',
+        dots: false,
+        centerMode: false,
+        infinite: false,
+        focusOnSelect: true
+    });
+
+    $('#sauces-container .products').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '#sauces-container .products-nav'
+    });
+    $('#sauces-container .products-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '#sauces-container .products',
+        dots: false,
+        centerMode: false,
+        infinite: false,
+        focusOnSelect: true
+    });
+
+    $('#drinks-container .products').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '#drinks-container .products-nav'
+    });
+    $('#drinks-container .products-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        asNavFor: '#drinks-container .products',
         dots: false,
         centerMode: false,
         infinite: false,
